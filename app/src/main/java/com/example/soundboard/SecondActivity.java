1b5250e18bbdf36a56b4155868e7d9f00b5e0159
Nathan Berumen <201158870@INV515581.lbusd.local>
2023-06-01 13:58:50 -0700

    Initial commit

---
+++ app/src/main/java/com/example/soundboard/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.soundboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    textView tvTopText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound_board2);
+    }
+}
